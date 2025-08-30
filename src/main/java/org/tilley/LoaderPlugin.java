9f9dba48c05f4e60d2a6c0ced0031590c0b2c71e
2kybe3 <kybe@kybe.xyz>
2025-08-30 14:49:03 +0200

    made it use packets and removed some nesting from code
    mostly bcs why wait until the next tick when receiving the packet alr
    
    act not sure how the order is but better use packets i guess

--- src/main/java/org/tilley/LoaderPlugin.java
+++ src/main/java/org/tilley/LoaderPlugin.java
@@ -4,20 +4,16 @@ import org.rusherhack.client.api.RusherHackAPI;
 import org.rusherhack.client.api.plugin.Plugin;
 
 public class LoaderPlugin extends Plugin {
-	
 	@Override
 	public void onLoad() {
-		
 		this.getLogger().info("Loaded InstantLoadModule plugin!");
 
 		final InstantLoadModule InstantLoadModule = new InstantLoadModule();
 		RusherHackAPI.getModuleManager().registerFeature(InstantLoadModule);
-
 	}
 
 	@Override
 	public void onUnload() {
 		this.getLogger().info("InstantLoadModule plugin unloaded!");
 	}
-	
 }
